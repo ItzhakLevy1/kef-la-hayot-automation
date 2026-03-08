289f5024d7e49b2fd93bbc9af0d5f9d85f48ca74
Itzhak Levy <itzhaklevydev@gmail.com>
2026-03-08 18:05:21 +0200

    Add a screenshoot of Bug 11 to allure report

--- src/test/java/tests/ContactCaptchaTest.java
+++ src/test/java/tests/ContactCaptchaTest.java
@@ -3,6 +3,7 @@ package tests;
 import io.qameta.allure.*;
 import org.testng.annotations.Test;
 import pages.ContactPage;
+import utils.AllureAttachments;
 
 public class ContactCaptchaTest extends BaseTest{
 
@@ -12,11 +13,13 @@ public class ContactCaptchaTest extends BaseTest{
     @Severity(SeverityLevel.NORMAL)
     @Feature("Contact page UI")
     @Owner("Itzhak Levy")
-    @Description("TC-11: Verify reCAPTCHA positioning to ensure no visual overlap with footer links")
+    @Description("TC-11: Verify reCAPTCHA positioning to ensure no visual overlap with near links")
     public void verifyCaptcha(){
         contactPage = new ContactPage(driver);
 
         contactPage.navigateToContactsPage();
         contactPage.highlightCaptchaIssue();
+
+        AllureAttachments.saveScreenshot("Contact Page UI - Verify reCAPTCHA positioning to ensure no visual overlap with near links", driver);
     }
 }
